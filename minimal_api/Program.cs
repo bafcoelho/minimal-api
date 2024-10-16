d33919d4e6cc8af3ad72dc1ac39c779bae9111fb
Bruno Coelho <bafcoelho@gmail.com>
2024-10-15 21:44:54 -0300

    feat:GET para retornar veículos

--- minimal_api/Program.cs
+++ minimal_api/Program.cs
@@ -55,8 +55,16 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     };
 
     veiculoServico.Incluir(veiculo);
-    
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+
 });
+
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico)=>{
+    var veiculos = veiculoServico.Todos(pagina);
+    return Results.Ok(veiculos);
+});
+
 
 #endregion
 
